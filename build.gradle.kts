69e8535032ba1b8270d8899cb726431316b1b0fe
ignite-jihyun-kim <solarb@ignite.co.kr>
2023-11-30 17:25:37 +0900

    framework-research-solarb-1 sonarqube github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,3 +11,16 @@ allprojects {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "ignite-jihyun_framework-research-solarb")
+        property("sonar.organization", "ignite-jihyun")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.gradle.skipCompile", true)
+    }
+}
